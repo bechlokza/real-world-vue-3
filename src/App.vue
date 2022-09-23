6cc51fb9821ac20c49174bf6cde1d00f8fc52737
Jesper Bech Loekza <jesper.lokza@valtech.com>
2022-09-23 15:09:13 +0200

    props and bindings

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,17 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+h4 {
+  font-size: 20px;
 }
 </style>
